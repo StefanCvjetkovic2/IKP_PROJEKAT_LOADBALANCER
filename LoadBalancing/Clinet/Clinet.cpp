8a05ed8b5fce3d6ca2d7a7f5b89e6acc096c926c
StefanCvjetkovic2 <cvjetkovic.pr34.2021@uns.ac.rs>
2024-11-29 14:50:10 +0100

    Client-LB connection

---
+++ LoadBalancing/Clinet/Clinet.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <cstdlib> // Za rand() i srand()
+#include <ctime>   // Za time()
+
+#pragma comment(lib, "ws2_32.lib") // Linkovanje WinSock biblioteke
+
+// Definišemo alias za cout
+auto& cout = std::cout;
+
+// Parametri servera (Load Balancera)
+#define SERVER_PORT 8080
+#define SERVER_IP "127.0.0.1"
+
+int brojac = 0;
+
+int main() {
+    // Inicijalizacija WinSock-a
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "WSAStartup failed\n";
+        return 1;
+    }
+
+    // Kreiranje soketa
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cout << "Socket creation failed\n";
+        WSACleanup();
+        return 1;
+    }
+
+    // Definisanje adrese servera (Load Balancera)
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
+
+    // Konekcija na Load Balancer
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        cout << "Connection to Load Balancer failed\n";
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Connected to Load Balancer\n";
+    int sendResult;
+    int randomNum;
+    // Generisanje i slanje slučajnih brojeva
+    srand(static_cast<unsigned>(time(0))); // Inicijalizacija generatora slučajnih brojeva
+    while (true) {
+        if (brojac < 20) {
+            randomNum = rand() % 100; // Broj između 0 i 99
+            sendResult = send(clientSocket, reinterpret_cast<char*>(&randomNum), sizeof(randomNum), 0);
+            brojac ++;
+        }
+        else {
+            brojac = 0;
+            cout << "Prvi zahtjev je poslat\n";
+            Sleep(60000);
+            
+
+        }
+        if (sendResult == SOCKET_ERROR) {
+            cout << "Failed to send data\n";
+            break;
+        }
+
+        cout << "Sent: " << randomNum << "\n";
+        Sleep(1000); // Pauza od 1 sekunde
+    }
+
+    // Zatvaranje konekcije i čišćenje resursa
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
--- LoadBalancing/LoadBalancer/LoadBalancer.cpp
+++ LoadBalancing/LoadBalancer/LoadBalancer.cpp
@@ -1,20 +1,240 @@
-// LoadBalancer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿#include "clientcommunication.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+auto& cout = std::cout;
+
+int main() {
+    // Inicijalizacija WinSock-a
+    if (!initializeWinSock()) {
+        return 1;
+    }
+
+    // Kreiranje i podešavanje server soketa za klijente
+    SOCKET serverSocket = setupClientSocket();
+    if (serverSocket == INVALID_SOCKET) {
+        cleanupWinSock();
+        return 1;
+    }
+
+    // Prihvatanje klijentske konekcije
+    SOCKET clientSocket = acceptClient(serverSocket);
+    if (clientSocket == INVALID_SOCKET) {
+        closesocket(serverSocket);
+        cleanupWinSock();
+        return 1;
+    }
+
+    // Primanje podataka od klijenta
+    int receivedNumber;
+    while (true) {
+        int bytesReceived = receiveFromClient(clientSocket, receivedNumber);
+        if (bytesReceived == SOCKET_ERROR || bytesReceived == 0) {
+            cout << "Client disconnected\n";
+            break;
+        }
+
+        cout << "Received from client: " << receivedNumber << "\n";
+    }
+
+    // Zatvaranje klijentskog soketa
+    closeClientSocket(clientSocket);
+
+    // Zatvaranje server soketa i čišćenje resursa
+    closesocket(serverSocket);
+    cleanupWinSock();
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//#include <vector>
+//#include <thread>
+//#include <mutex>
+//
+//#pragma comment(lib, "ws2_32.lib")
+//
+//auto& cout = std::cout;
+//
+//#define SERVER_PORT 8080
+//#define WORKER_PORT 8081
+//#define BUFFER_SIZE 4
+//
+//std::vector<SOCKET> workers; // Lista Worker soketa
+//std::mutex workerMutex;      // Mutex za bezbedan pristup listi Workera
+//
+//// Funkcija za rukovanje konekcijama sa Workerima
+//void acceptWorkers(SOCKET workerSocket) {
+//    sockaddr_in workerClientAddr{};
+//    int workerClientAddrSize = sizeof(workerClientAddr);
+//
+//    while (true) {
+//        SOCKET workerClientSocket = accept(workerSocket, (sockaddr*)&workerClientAddr, &workerClientAddrSize);
+//        if (workerClientSocket == INVALID_SOCKET) {
+//            cout << "Failed to accept worker connection\n";
+//            continue;
+//        }
+//
+//        // Dodavanje novog Workera u listu
+//        {
+//            std::lock_guard<std::mutex> lock(workerMutex);
+//            workers.push_back(workerClientSocket);
+//        }
+//        cout << "Worker connected. Total workers: " << workers.size() << "\n";
+//    }
+//}
+//
+//int main() {
+//    WSADATA wsaData;
+//    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+//        cout << "WSAStartup failed\n";
+//        return 1;
+//    }
+//
+//    // Kreiranje TCP soketa za Load Balancer
+//    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+//    if (serverSocket == INVALID_SOCKET) {
+//        cout << "Socket creation failed\n";
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Konfiguracija adrese Load Balancera za klijente
+//    sockaddr_in serverAddr{};
+//    serverAddr.sin_family = AF_INET;
+//    serverAddr.sin_port = htons(SERVER_PORT);
+//    serverAddr.sin_addr.s_addr = INADDR_ANY;
+//
+//    // Bindovanje i slušanje za klijente
+//    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+//        cout << "Bind failed\n";
+//        closesocket(serverSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//    if (listen(serverSocket, SOMAXCONN) < 0) {
+//        cout << "Listen failed\n";
+//        closesocket(serverSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//    cout << "Load Balancer is listening for clients on port " << SERVER_PORT << "\n";
+//
+//    // Kreiranje TCP soketa za Workere
+//    SOCKET workerSocket = socket(AF_INET, SOCK_STREAM, 0);
+//    if (workerSocket == INVALID_SOCKET) {
+//        cout << "Worker socket creation failed\n";
+//        closesocket(serverSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Konfiguracija adrese za Workere
+//    sockaddr_in workerAddr{};
+//    workerAddr.sin_family = AF_INET;
+//    workerAddr.sin_port = htons(WORKER_PORT);
+//    workerAddr.sin_addr.s_addr = INADDR_ANY;
+//
+//    // Bindovanje i slušanje za Workere
+//    if (bind(workerSocket, (sockaddr*)&workerAddr, sizeof(workerAddr)) < 0) {
+//        cout << "Worker bind failed\n";
+//        closesocket(workerSocket);
+//        closesocket(serverSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//    if (listen(workerSocket, SOMAXCONN) < 0) {
+//        cout << "Worker listen failed\n";
+//        closesocket(workerSocket);
+//        closesocket(serverSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//    cout << "Load Balancer is listening for workers on port " << WORKER_PORT << "\n";
+//
+//    // Startovanje niti za prihvatanje Workera
+//    std::thread workerThread(acceptWorkers, workerSocket);
+//    workerThread.detach();
+//
+//    // Prihvatanje klijentske konekcije
+//    sockaddr_in clientAddr{};
+//    int clientAddrSize = sizeof(clientAddr);
+//    SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientAddrSize);
+//    if (clientSocket == INVALID_SOCKET) {
+//        cout << "Failed to accept client connection\n";
+//        closesocket(workerSocket);
+//        closesocket(serverSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//    cout << "Client connected\n";
+//
+//    // Primanje podataka od klijenta i prosleđivanje Workerima
+//    int receivedNumber;
+//    while (true) {
+//        int bytesReceived = recv(clientSocket, reinterpret_cast<char*>(&receivedNumber), BUFFER_SIZE, 0);
+//        if (bytesReceived == SOCKET_ERROR || bytesReceived == 0) {
+//            cout << "Client disconnected\n";
+//            break;
+//        }
+//
+//        cout << "Received from client: " << receivedNumber << "\n";
+//
+//        // Prosleđivanje primljenog broja Workerima
+//        std::lock_guard<std::mutex> lock(workerMutex);
+//        for (auto it = workers.begin(); it != workers.end(); ) {
+//            int sendResult = send(*it, reinterpret_cast<char*>(&receivedNumber), sizeof(receivedNumber), 0);
+//            if (sendResult == SOCKET_ERROR) {
+//                cout << "Worker disconnected\n";
+//                closesocket(*it);
+//                it = workers.erase(it); // Ukloni Workera ako je prekinuo vezu
+//            }
+//            else {
+//                cout << "Forwarded to worker\n";
+//                ++it;
+//            }
+//        }
+//    }
+//
+//    // Zatvaranje svih soketa i čišćenje resursa
+//    {
+//        std::lock_guard<std::mutex> lock(workerMutex);
+//        for (auto& worker : workers) {
+//            closesocket(worker);
+//        }
+//    }
+//    closesocket(clientSocket);
+//    closesocket(serverSocket);
+//    closesocket(workerSocket);
+//    WSACleanup();
+//
+//    return 0;
+//}
